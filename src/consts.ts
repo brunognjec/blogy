87de04b92cfe46caa62e879c18b1965ddd9cdcc8
“brunkiiii” <brunki294@gmail.com>
2024-01-17 13:56:29 +0100

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Blogy";
+export const SITE_DESCRIPTION =
+  "Achieving focus by delving into someones journey and struggles with concentration through reading.";
